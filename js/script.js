69facb564be280eecc924344b62a8a4038d0d244
Сергей <sindenovs@live.com>
2018-12-17 01:35:47 +0300

    добавил скрипты, анимации, минифицировал JS и CSS

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var modal_button = document.querySelector(".form_label_button");
+var hotel_form = document.querySelector(".hotel_find");
+var arrive = document.querySelector("[name=arrive]");
+var departure = document.querySelector("[name=departure]");
+var adult = document.querySelector("[name=adult");
+
+modal_button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  hotel_form.classList.toggle("hotel_find_hide");
+  arrive.focus();
+  arrive.classList.remove("error");
+  departure.classList.remove("error");
+  adult.classList.remove("error");
+});
+
+hotel_form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  if (!arrive.value || !departure.value || !adult.value) {
+      arrive.classList.remove("error");
+      departure.classList.remove("error");
+      adult.classList.remove("error");
+      hotel_form.offsetWidth = hotel_form.offsetWidth;
+      arrive.classList.add("error");
+      departure.classList.add("error");
+      adult.classList.add("error");
+ 
+
+  };
+});
